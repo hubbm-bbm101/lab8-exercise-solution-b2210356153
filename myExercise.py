049c87d1af40c19b187ae8c7e24b83b7fd601f58
b2210356153 <92568603+b2210356153@users.noreply.github.com>
2021-12-03 21:01:36 +0300

    Update myExercise.py

--- myExercise.py
+++ myExercise.py
@@ -12,8 +12,7 @@ for item in sys.argv[2].split(","):
     try:
         university = student_info[item][0]
         department = student_info[item][1]
-        print("Name: "
-        str(item) + "\nUniversity: " + str(university) + "\nDepartment: " + str(department))
+        print("Name: " + str(item) + "\nUniversity: " + str(university) + "\nDepartment: " + str(department))
 
         except KeyError:
         print("\nNo record of '{}' was found".format(item))
